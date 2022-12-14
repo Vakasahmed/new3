54c48d391d8a275703cdbc453851dcbdd2096ae2
Shaikh Vakas Ahmed <97548328+Vakasahmed@users.noreply.github.com>
2022-12-14 18:44:04 +0530

    Add files via upload

---
+++ src/Components/Profile.js
@@ -0,0 +1,58 @@
+import React from "react";
+
+ const Profile = () => {
+  return (
+    <div>
+      <div>
+        <h2>Bittoo Agrawal</h2>
+        <p>Software Developer,Educator at Geekster </p>
+        <a href="_blank">
+          Linkedin 
+        </a>
+        <a href="_blank">
+           GitHub
+        </a>
+        <hr />
+      </div>
+      <div>
+        <h3>Experience</h3>
+        <p>Software Developer at Amazon</p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo vitae vel
+          quae illum fuga id culpa aliquid consectetur corrupti. Ullam
+          doloremque aperiam maiores iste perferendis laborum corrupti pariatur
+          reprehenderit numquam illum! Sunt unde hic dignissimos reprehenderit
+          molestias fugit natus nostrum!
+        </p>
+        <hr />
+        <p>Software Developer at Amazon</p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo vitae vel
+          quae illum fuga id culpa aliquid consectetur corrupti. Ullam
+          doloremque aperiam maiores iste perferendis laborum corrupti pariatur
+          reprehenderit numquam illum! Sunt unde hic dignissimos reprehenderit
+          molestias fugit natus nostrum!
+        </p>
+        <hr />
+        <h2>Skills</h2>
+        <ul>
+          <li>Java</li>
+          <li>JavaScript</li>
+          <li>API</li>
+          <li>React</li>
+          <li>System design</li>
+        </ul>
+        <hr />
+        <h2>Hobbies</h2>
+        <ol>
+          <li>Table tennis</li>
+          <li>Reading Books</li>
+          <li>Biography</li>
+          <li>Teaching</li>
+        </ol>
+      </div>
+    </div>
+  );
+}
+
+export default Profile;
